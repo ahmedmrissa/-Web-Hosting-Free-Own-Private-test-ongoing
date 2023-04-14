3214fc39a02d7859d3b73bd09e3f4cf5100843d9
Ahmed Mrissa <amrissa1@gmail.com>
2023-04-13 23:03:58 -0400

    upload using firebase

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -85,7 +85,7 @@ router.put('/upload/:id', async (req, res, next) => {
             await uploadBytes(imageRef, upLoadedPhoto.buffer, metatype)
 
 
-            let photoUrl = await storage().ref('uploads/' + path).getDownloadURL();
+            let photoUrl = await storageRef.ref('uploads/' + path).getDownloadURL();
 
 
 
