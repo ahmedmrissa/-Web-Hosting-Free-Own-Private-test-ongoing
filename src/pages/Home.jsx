b89a51bfac25ab30c46a3bd2f38f1566f947f195
Ahmed Mrissa <amrissa1@gmail.com>
2023-04-19 22:20:12 -0400

    Implement Search+selection home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import ProductCard from '../components/product/ProductCard';
 import useGetProduct from '../hooks/useProducts';
-import { search } from '../../controllers/product.controller';
+
+
 
 
 function Home() {
@@ -10,24 +11,17 @@ function Home() {
  const { products } = useGetProduct()
  
 
-if( !products || !products.length )
-{
-  return (<div>  <button type="button" disabled className="text-white bg-blue-700 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-red-700 dark:focus:ring-blue-800 inline-flex items-center"
-                     
-  >
-    <svg aria-hidden="true"  role="status" className="inline w-4 h-4 mr-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
-      <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
-    </svg>
-    Loading ...
-  </button>
-  </div>)
-}
-else{
+
+
+
   const [prod, setProd] = useState([]);
-  const [search, setSearch] = useState();
+  const [search, setSearch] = useState(null);
+  const [selection, setSelection] = useState(null)
+  const [refresh, setRefresh] = useState(false)
+  const [selectionPrice, setSelectionPrice] = useState("")
+  
   useEffect(() => {
-    if(selection!==null){
+   
    const SelectProduct = ()=>{
       let tmp= [...products];
       
@@ -41,84 +35,198 @@ else{
     
     
     SelectProduct()
-  }else{
-       const SearchProduct = ()=>{
-      let tmp= [...products];
-      
-     
-      
-      tmp=tmp.filter(t=>t.category===search||t.label===search ||t.brand===search || t.price===search)
-      
-     setProd(tmp)
-     
-    }
-    
-    
-    SearchProduct() }
-  }, [selection,search])
-  return (
-    
-    
-      
-    
-    <div className='flex flex-wrap self-center justify-center flex-grow gap-2 p-2 m-1 md:flex-row sm:flex-col'>
-      <select className='w-1/2 p-1 m-1 font-mono text-gray-800 rounded-md border-spacing-2 ' value={selection}
-         onChange={(e) => {
-          
-          
-          setSelection(e.target.value);
-        }
-        }>
-          <option value="Smart Phone" >Smart Phone</option>
-          <option value="Phone" >Phone</option>
-          <option value="Tablet" >Tablet</option>
-          <option value="Tablet" >Laptop</option>
-      </select>
-      <input
-        name="search"
-        className='w-1/2 p-1 m-1 font-mono text-gray-800  text-center rounded-md border-spacing-2 p-2 m-3'
-        value={search}
-        onChange={(e) => {
-          setSearch( e.target.value)
-        }
-      }/>
-        
-    
-      <td className="px-6 py-4"> <button type="button" disabled className="text-white bg-blue-700 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-red-700 dark:focus:ring-blue-800 inline-flex items-center"
-                     
-                     >
-                       <svg aria-hidden="true" role="status" className="inline w-4 h-4 mr-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                         <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
-                         <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
-                       </svg>
-                       {order.user.fullName}
-                     </button>
-                     </td>
-      { !prod ?
-        products.map(product=>(
-          <ProductCard key={product._id} 
-          id={product._id}
-          label={product.label} 
-          brand={product.brand}
-          category={product.category}
-          price={product.price}
-          photo_url={product.photo_url || 'https://guide-images.cdn.ifixit.com/igi/o4OjCNmNeOhvsS1P.standard'}
-        />)) : prod.map(product=>(
-          <ProductCard key={product._id} 
-          id={product._id}
-          label={product.label} 
-          brand={product.brand}
-          category={product.category}
-          price={product.price}
-          photo_url={product.photo_url || 'https://guide-images.cdn.ifixit.com/igi/o4OjCNmNeOhvsS1P.standard'}
-        />))
-      }
-      
-    </div>
   
-  )
+     
+    
+    
+    
+  }, [selection,refresh])
+
+
+  const searchProduct=async ()=>{
+    let tmp= [...products];
+      
+     
+      
+    tmp=tmp.filter(t=>t.category===search||t.label===search ||t.brand===search || t.price===Number(search))
+    
+   setProd(tmp)
+   
+  }
+  if(selection===null && search===null){  
+    return (
+      
+      <>
+      
+        <div >
+          <div className='flex flex-row'>
+          <div className='w-1/2 p-1 m-1 font-mono text-gray-800 rounded-md border-spacing-2 '>
+            
+        <select className='w-1/2 p-1 m-1 font-mono text-gray-800 rounded-md border-spacing-2 ' value={selection}
+           onChange={(e) => {
+            
+            
+            setSelection(e.target.value);
+          }
+          }>
+            <option value="Smart Phone" >Smart Phone</option>
+            <option value="Phone" >Phone</option>
+            <option value="Tablet" >Tablet</option>
+            <option value="Laptop" >Laptop</option>
+        </select>
+     </div>
+     <div className='w-1/2 p-1 m-1 font-mono text-gray-800 rounded-md border-spacing-2 '>
+        <select className='w-1/2 p-1 m-1 font-mono text-gray-800 rounded-md border-spacing-2 ' value={selectionPrice}
+           onChange={(e) => {
+            
+            
+            setSelectionPrice(e.target.value);
+          }
+          }>
+            <option value="Phone" >Show me from Low to High  Price</option>
+            <option value="Smart Phone" >Show me from High to Low Price</option>
+            
+           
+        </select>
+        </div>
+        
+  
+  <form>   
+      <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+      <div className="relative">
+          <div className="absolute inset-y-3 right-1/2  flex items-center pl-3  pointer-events-none">
+              <svg aria-hidden="true" className="w-5 h-5   text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+          </div>
+          
+          <input type="search" id="default-search"  value={search}
+     
+          onChange={(e) => {
+            setSearch( e.target.value)
+           
+          }
+        } className="block  w-full p-2 pl-10 text-xl text-blue-700 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="      Search..." required />
+          
+          <button type="button" onClick={() => {
+      
+                        searchProduct()
+                      }} className="text-white absolute right-2.5 bottom-1 bg-blue-700 hover:bg-blue-800  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+      </div>
+  </form>
+  
+  
+  </div>  
+        
+       
+         
+                       </div>           
+      
+      
+      
+      <div className='flex flex-wrap self-center justify-center flex-grow gap-2 p-2 m-1 md:flex-row sm:flex-col'>
+        
+       
+          {             
+       
+          products.map(product=>(
+            <ProductCard key={product._id} 
+            id={product._id}
+            label={product.label} 
+            brand={product.brand}
+            category={product.category}
+            price={product.price}
+            photo_url={product.photo_url || 'https://guide-images.cdn.ifixit.com/igi/o4OjCNmNeOhvsS1P.standard'}
+          />))}
+          </div>
+      </>)}
+      else{
+        return (
+      
+          <>
+          
+            <div >
+              <div className='flex flex-row'>
+              <div className='w-1/2 p-1 m-1 font-mono text-gray-800 rounded-md border-spacing-2 '>
+                
+            <select className='w-1/2 p-1 m-1 font-mono text-gray-800 rounded-md border-spacing-2 ' value={selection}
+               onChange={(e) => {
+                
+                
+                setSelection(e.target.value);
+              }
+              }>
+                <option value="Smart Phone" >Smart Phone</option>
+                <option value="Phone" >Phone</option>
+                <option value="Tablet" >Tablet</option>
+                <option value="Laptop" >Laptop</option>
+            </select>
+         </div>
+         <div className='w-1/2 p-1 m-1 font-mono text-gray-800 rounded-md border-spacing-2 '>
+            <select className='w-1/2 p-1 m-1 font-mono text-gray-800 rounded-md border-spacing-2 ' value={selectionPrice}
+               onChange={(e) => {
+                
+                
+                setSelectionPrice(e.target.value);
+              }
+              }>
+                <option value="Phone" >Show me from Low to High  Price</option>
+                <option value="Smart Phone" >Show me from High to Low Price</option>
+                
+               
+            </select>
+            </div>
+            
+      
+      <form>   
+          <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+          <div className="relative">
+              <div className="absolute inset-y-3 right-1/2  flex items-center pl-3  pointer-events-none">
+                  <svg aria-hidden="true" className="w-5 h-5   text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+              </div>
+              
+              <input type="search" id="default-search"  value={search}
+         
+              onChange={(e) => {
+                setSearch( e.target.value)
+               
+              }
+            } className="block  w-full p-2 pl-10 text-xl text-blue-700 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="      Search..." required />
+              
+              <button type="button" onClick={() => {
+          
+                            searchProduct()
+                          }} className="text-white absolute right-2.5 bottom-1 bg-blue-700 hover:bg-blue-800  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+          </div>
+      </form>
+      
+      
+      </div>  
+            
+           
+             
+                           </div>           
+          
+          
+          
+          <div className='flex flex-wrap self-center justify-center flex-grow gap-2 p-2 m-1 md:flex-row sm:flex-col'>
+            
+           
+              {             
+           
+              prod.map(product=>(
+                <ProductCard key={product._id} 
+                id={product._id}
+                label={product.label} 
+                brand={product.brand}
+                category={product.category}
+                price={product.price}
+                photo_url={product.photo_url || 'https://guide-images.cdn.ifixit.com/igi/o4OjCNmNeOhvsS1P.standard'}
+              />))}
+              </div>
+          </>)}
+        
     }
-}
+  
+
 
 export default Home
 
